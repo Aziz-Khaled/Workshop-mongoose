cd05b75912b7ead7d7e08a4883c3c74ac8d616df
Aziz-Khaled <mouhamedazizkhaled@gmail.com>
2022-02-07 17:19:41 +0100

    workshop

---
+++ Models/contact.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const Contact =  new mongoose.Schema (
+
+{
+    name : { type : String , required : true}  , 
+    email :  { type : String , required : true , unique : true} , 
+    age :  { type : Number } , 
+},
+)
+
+module.exports = mongoose.model ('User', Contact)
